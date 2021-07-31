ac49797feb4f40c926032419cb4156ac04836ee1
Yining (Johnny) Wang <yining.wang@mail.utoronto.ca>
2021-07-30 20:35:27 -0400

    data transfer from MongoDB to website complete

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, redirect 
 from flask_pymongo import PyMongo 
+import sys
+import json
 import process_mongo_data
+import plotly
+import plotly.graph_objects as go
 
 # Use Flask to create app
 app = Flask(__name__)
@@ -19,7 +23,22 @@ def ini():
 @app.route("/update_RDR6")
 def update_RDR6():
     RDR6_plot = mongo.db.RDR6_Txcal_Prod
-    RDR6_plot_new = process_mongo_data.process()
+    Avg_test_time, Pass_rate, Uni_date, Antennal1_tx_power, Antenna2_tx_power = process_mongo_data.process()
+    gauge_fig = go.Figure(go.Indicator(
+        domain = {'x': [0, 1], 'y':[0, 1]},
+        value = Avg_test_time,
+        mode = "gauge+number",
+        title = {'text' : "Average Test Time"},
+        gauge = {'axis' : {'range' : [None, 500]},
+                'steps' : [
+                    {'range' : [0, 25], 'color' : 'lightgray'},
+                    {'range' : [250, 400], 'color' : 'gray'}],
+                'threshold' : {'line' : {'color' : 'red', 'width' : 4}, 'thickness' : 0.75, 'value': 45}}
+                ))
+    gaugeJSON = json.dumps(gauge_fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('Data_Dashboard.html', gaugeJSON=gaugeJSON, test=Avg_test_time)
+                
 
 if __name__ == "__main__":
     app.run()
--- process_mongo_data.py
+++ process_mongo_data.py
@@ -1,5 +1,55 @@
-import pymongo
-import pandas as pd
+# Import library
+from pymongo import MongoClient
+import numpy as np
 
 # Process data in MongoDB for plotting
 def process():
+    # Initialization
+    test_date = list()
+    pass_fail = list()
+    test_duration = list()
+    antenna1_tx_power = list()
+    antenna2_tx_power = list()
+
+    # Create connection
+    Client = MongoClient("mongodb://localhost:27017/")
+
+    # Database
+    db = Client.Veoneer_Data_Project
+
+    # Collection
+    col = db.RDR6_TxCal_Prod
+
+    # Point the cursor
+    x = col.find()
+
+    for item in x:
+        index_list = list(item.keys())
+    index_list.remove('_id')
+
+    # Point the cursor
+    x = col.find()
+
+    # Collect all the needed data info
+    for item in list(x):
+        for index in index_list:
+            test_date.append(item[index]['TestDate'])
+            pass_fail.append(item[index]['Pass/Fail'])
+            test_duration.append(item[index]['Test Time'])
+            antenna1_tx_power.append(item[index]['Antenna1 GetTxPower'])
+            antenna2_tx_power.append(item[index]['Antenna2 GetTxPower'])
+
+    # calculate the average test time
+    avg_test_time = np.average(test_duration)
+
+    # calculate the passing rate
+    pass_rate = pass_fail.count('PASS') / (pass_fail.count('FAIL') + pass_fail.count('PASS'))
+
+    # Keep unique date values 
+    uni_date = list()
+    for date in test_date:
+        if date not in uni_date:
+            uni_date.append(date)
+
+    # return results
+    return avg_test_time, pass_rate, uni_date, antenna1_tx_power, antenna2_tx_power
